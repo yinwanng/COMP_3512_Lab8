c709e8a4b9425694b9d50a0bdd760598daab8f75
yinwanng <xxjohnnyng@gmail.com>
2018-03-28 12:35:18 -0700

    get method

--- Lab8/Lab8/FixedList.h
+++ Lab8/Lab8/FixedList.h
@@ -3,13 +3,41 @@ template <class T, size_t N>
 class FixedList
 {
 public:
+	FixedList();
+	~FixedList();
 	const T& get(unsigned int index) const;
-	T& operator[](unsigned int index);
-	int getFirstIndex(const T& t) const;
-	size_t size() const;
+	//T& operator[](unsigned int index);
+	//int getFirstIndex(const T& t) const;
+	//size_t size() const;
 	bool add(const T& t);
-	T remove(const T& t);
+	//T remove(const T& t);
 private:
-	T a [N];
+	T contents [N];
+	size_t size;
+	size_t capacity = N;
 };
+
+template<class T, size_t N>
+inline FixedList<T, N>::FixedList()
+{
+	size = 0;
+}
+
+template<class T, size_t N>
+inline FixedList<T, N>::~FixedList()
+{
+}
+
+template<class T, size_t N>
+inline const T & FixedList<T, N>::get(unsigned int index) const
+{
+	if (index < size)
+	{
+		return contents[index];
+	}
+	else {
+		throw std::range_error("Out of range.");
+	}
+}
+
 
--- Lab8/Lab8/main.cpp
+++ Lab8/Lab8/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "FixedList.h"
 using namespace std;
 
 int main()
 {
+	FixedList<int, 5> test;
+
+	cout << test.get(5) << endl;
+
+
 	system("pause");
 	return 0;
 }
