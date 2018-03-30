2b911100e95e197ae64d0c47f3cbd7d7fcb4ba2d
yinwanng <xxjohnnyng@gmail.com>
2018-03-30 13:11:09 -0700

    Test output

--- Lab8/Lab8/FixedList.h
+++ Lab8/Lab8/FixedList.h
@@ -11,17 +11,18 @@ public:
 	size_t size() const;
 	size_t capacity() const;
 	bool add(const T& t);
-	//T remove(const T& t);
+	T remove(const T& t);
 private:
 	T contents [N];
-	size_t size;
-	size_t capacity = N;
+	size_t cSize;
+	size_t cCapacity;
 };
 
 template<class T, size_t N>
 inline FixedList<T, N>::FixedList()
 {
-	size = 0;
+	cSize = 0;
+	cCapacity = N;
 }
 
 template<class T, size_t N>
@@ -32,7 +33,7 @@ inline FixedList<T, N>::~FixedList()
 template<class T, size_t N>
 inline const T & FixedList<T, N>::get(unsigned int index) const
 {
-	if (index < size)
+	if (index < cSize)
 	{
 		return contents[index];
 	}
@@ -44,7 +45,7 @@ inline const T & FixedList<T, N>::get(unsigned int index) const
 template<class T, size_t N>
 inline T & FixedList<T, N>::operator[](unsigned int index)
 {
-	if (index < size)
+	if (index < cSize)
 	{
 		return contents[index];
 	}
@@ -56,8 +57,14 @@ inline T & FixedList<T, N>::operator[](unsigned int index)
 template<class T, size_t N>
 inline int FixedList<T, N>::getFirstIndex(const T & t) const
 {
-	if (size > 0) {
-		return 0;
+	if (cSize > 0) {
+		for (int i = 0; i < cSize; i++)
+		{
+			if (contents[i] == t) 
+			{
+				return i;
+			}
+		}
 	}
 	return -1;	
 }
@@ -66,14 +73,42 @@ inline int FixedList<T, N>::getFirstIndex(const T & t) const
 template<class T, size_t N>
 inline size_t FixedList<T, N>::size() const
 {
-	return size;
+	return cSize;
 }
 
 template<class T, size_t N>
 inline size_t FixedList<T, N>::capacity() const
 {
-	return capacity;
+	return cCapacity;
 }
+
+template<class T, size_t N>
+inline bool FixedList<T, N>::add(const T & t)
+{
+	if (cSize < cCapacity)
+	{
+		contents[cSize++] = t;
+		return true;
+	}
+	return false;
+}
+
+template<class T, size_t N>
+inline T FixedList<T, N>::remove(const T & t)
+{
+	int index = getFirstIndex(t);
+
+	if (index >= 0) {
+		for (int i = index; i < cSize; i++) {
+			contents[i] = contents[i + 1];
+		}
+		--cSize;
+		return t;
+	}
+	return NULL;
+}
+
+
 
 
 
--- Lab8/Lab8/main.cpp
+++ Lab8/Lab8/main.cpp
@@ -1,14 +1,91 @@
 #include <iostream>
 #include "FixedList.h"
+#include <string>
 using namespace std;
 
 int main()
 {
-	FixedList<int, 5> test;
-	//cout << test.get(0) << endl;
-	//cout << test[0] << endl;
+	// Construct a double FixedList
+	FixedList<double, 6> list1;
 
+	// Add double values
+	list1.add(1.0);
+	list1.add(2.0);
+	list1.add(3.0);
+	list1.add(4.0);
+	list1.add(5.0);
+	cout << "Doubles: " << endl;
+
+	// Size / Capacity
+	cout << "Size: " << list1.size() << endl;
+	cout << "Capacity: " << list1.capacity() << endl;
+
+	// Get index value
+	cout << "Index 0: " << list1.get(0) << endl;
+	cout << "Index 1: " << list1.get(1) << endl;
+	cout << "Index 2: " << list1.get(2) << endl;
+	cout << "Index 3: " << list1.get(3) << endl;
+	cout << "Index 4: " << list1.get(4) << endl;
+
+	// []
+	cout << "list1[0] " << list1[0] << endl;
+	cout << "list1[1] " << list1[1] << endl;
+	cout << "list1[2] " << list1[2] << endl;
+	cout << "list1[3] " << list1[3] << endl;
+	cout << "list1[4] " << list1[4] << endl;
+
+
+	// Get first index
+	cout << "Find 3 at index: " << list1.getFirstIndex(3) << endl;
+	cout << "Find 12 at index: " << list1.getFirstIndex(12) << endl;
+
+	// Remove
+	list1.remove(1);
+	cout << "list1[0] " << list1[0] << endl;
+	cout << "list1[1] " << list1[1] << endl;
+	cout << "list1[2] " << list1[2] << endl;
+	cout << "list1[3] " << list1[3] << endl;
+	cout << endl << endl;
+	// Construct a string FixedList
+	FixedList<string, 6> list2;
+
+	// Add string values
+	list2.add("Johnny");
+	list2.add("Bob");
+	list2.add("Chris");
+	list2.add("Fiona");
+	cout << "Strings: " << endl;
+
+	// Size / Capacity
+	cout << "Size: " << list2.size() << endl;
+	cout << "Capacity: " << list2.capacity() << endl;
+
+	// Get index value
+	cout << "Index 0: " << list2.get(0) << endl;
+	cout << "Index 1: " << list2.get(1) << endl;
+	cout << "Index 2: " << list2.get(2) << endl;
+	cout << "Index 3: " << list2.get(3) << endl;
+
+	// []
+	cout << "list2[0] " << list2[0] << endl;
+	cout << "list2[1] " << list2[1] << endl;
+	cout << "list2[2] " << list2[2] << endl;
+	cout << "list2[3] " << list2[3] << endl;
+
+	// Get first index
+	cout << "Find Chris at index: " << list2.getFirstIndex("Chris") << endl;
+	cout << "Find Bill at index: " << list2.getFirstIndex("Bill") << endl;
+
+	// Remove
+	list2.remove("Chris");
+	cout << "list2[0] " << list2[0] << endl;
+	cout << "list2[1] " << list2[1] << endl;
+	cout << "list2[2] " << list2[2] << endl;
+	cout << endl << endl;
+	
 
 	system("pause");
 	return 0;
 }
+
+
